ccc2c99e2d811fb2583a10233ad61843b32553e4
Hawk <haakonhobaek@gmail.com>
2022-03-28 19:11:51 +0200

    Update points after round 2 with poker

--- src/scores.js
+++ src/scores.js
@@ -7,181 +7,211 @@ const participants = [
     {
         name: 'Andreas',
         results: [
-            1
+            1,
+            PARTICIPATED
         ],
     },
     {
         name: 'Ann Katrin',
         results: [
-            NOT_PARTICIPATED
+            NOT_PARTICIPATED,
+            PARTICIPATED
         ],
     },
     {
         name: 'Bård',
         results: [
-            NOT_PARTICIPATED
+            NOT_PARTICIPATED,
+            PARTICIPATED
         ],
     },
     {
         name: 'Eirik B',
         results: [
-            PARTICIPATED
+            PARTICIPATED,
+            NOT_PARTICIPATED
         ],
     },
     {
         name: 'Erik E',
         results: [
-            PARTICIPATED
+            PARTICIPATED,
+            NOT_PARTICIPATED
         ],
     },
     {
         name: 'Erik S',
         results: [
+            NOT_PARTICIPATED,
             NOT_PARTICIPATED
         ],
     },
     {
         name: 'Erlend',
         results: [
+            PARTICIPATED,
             PARTICIPATED
         ],
     },
     {
         name: 'Frederik',
         results: [
+            PARTICIPATED,
             PARTICIPATED
         ],
     },
     {
         name: 'Fredrik B',
         results: [
+            PARTICIPATED,
             PARTICIPATED
         ],
     },
     {
         name: 'Fredrik S',
         results: [
-            PARTICIPATED
+            PARTICIPATED,
+            1
         ],
     },
     {
         name: 'Gustav',
         results: [
-            NOT_PARTICIPATED
+            NOT_PARTICIPATED,
+            PARTICIPATED
         ],
     },
     {
         name: 'Hallvard',
         results: [
+            NOT_PARTICIPATED,
             NOT_PARTICIPATED
         ],
     },
     {
         name: 'Herman',
         results: [
-            2
+            2,
+            NOT_PARTICIPATED
         ],
     },
     {
         name: 'Håken',
         results: [
-            3
+            3,
+            PARTICIPATED
         ],
     },
     {
         name: 'Håkon',
         results: [
+            PARTICIPATED,
             PARTICIPATED
         ],
     },
     {
         name: 'Ismar',
         results: [
+            PARTICIPATED,
             PARTICIPATED
         ],
     },
     {
         name: 'Ivar',
         results: [
-            NOT_PARTICIPATED
+            NOT_PARTICIPATED,
+            3
         ],
     },
     {
         name: 'Jan Erik',
         results: [
-            NOT_PARTICIPATED
+            NOT_PARTICIPATED,
+            PARTICIPATED
         ],
     },
     {
         name: 'Jørgen',
         results: [
+            PARTICIPATED,
             PARTICIPATED
         ],
     },
     {
         name: 'Ken',
         results: [
-            PARTICIPATED
+            PARTICIPATED,
+            NOT_PARTICIPATED
         ],
     },
     {
         name: 'Lars Olav',
         results: [
-            NOT_PARTICIPATED
+            NOT_PARTICIPATED,
+            PARTICIPATED
         ],
     },
     {
         name: 'Marius',
         results: [
+            PARTICIPATED,
             PARTICIPATED
         ],
     },
     {
         name: 'Ole-André',
         results: [
-            PARTICIPATED
+            PARTICIPATED,
+            NOT_PARTICIPATED
         ],
     },
     {
         name: 'Ole Tommy',
         results: [
+            PARTICIPATED,
             PARTICIPATED
         ],
     },
     {
         name: 'Richard',
         results: [
-            NOT_PARTICIPATED
+            NOT_PARTICIPATED,
+            2
         ],
     },
     {
         name: 'Seán',
         results: [
-            NOT_PARTICIPATED
+            NOT_PARTICIPATED,
+            4
         ],
     },
     {
         name: 'Terje',
         results: [
+            PARTICIPATED,
             PARTICIPATED
         ],
     },
     {
         name: 'Tom Erik',
         results: [
+            PARTICIPATED,
             PARTICIPATED
         ],
     },
     {
         name: 'Tor Eric',
         results: [
+            PARTICIPATED,
             PARTICIPATED
         ],
     },
     {
         name: 'Vetle',
         results: [
-            4
+            4,
+            PARTICIPATED
         ],
     },
 ];
@@ -189,6 +219,11 @@ const participants = [
 export const rounds = [
     {
         head: 'Racing (2022)',
+        points: [500, 400, 300, 200],
+        participationScore: 100,
+    },
+    {
+        head: 'Poker (2022)',
         points: [500, 400, 300, 200],
         participationScore: 100,
     },
